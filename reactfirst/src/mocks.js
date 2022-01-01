8b0c2122984d4b7b42e8173a575235a848cbb84b
Lenovo <haymatit@post.bgu.ac.il>
2022-01-01 12:28:59 +0200

    delete mock.js

--- reactfirst/src/mocks.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-import MockAdapter from "axios-mock-adapter";
-
-const mock = new MockAdapter(axios);
-const data = { response: true };
-mock.onGet('http://localhost:3000/programManagers').reply(200, data);
