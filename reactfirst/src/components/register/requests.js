2f24ed3b25afd3d00efb42795f2d3cda6275d9ec
vaitz <vaitz.post.bgu.ac.il>
2022-01-05 19:18:57 +0200

    change request format

--- reactfirst/src/components/register/requests.js
+++ reactfirst/src/components/register/requests.js
@@ -9,20 +9,19 @@ mock.onPost(SERVER_ADDRESS+'/users/register/student').reply(201, 'A new user has
 // mock.onPost(serverAddress+'/users/register/student').reply(400, 'A user with the same username already exists');
 // mock.onPost(serverAddress+'/users/register/student').reply(500, 'Server internal error');
 
-export function sendDetailsToServer({userType, username, firstname, lastname, email, password}){
+export async function sendDetailsToServer({userType, username, firstname, lastname, email, password}){
     // need to hash the password in the server, suppose to return error if username exists (or other)
     if(userType === "סטודנט"){
-        axios.post(SERVER_ADDRESS+'/users/register/student',
+        const response = await axios.post(SERVER_ADDRESS+'/users/register/student',
         {
             "username": username,
             "firstName": firstname,
             "lastName": lastname,
             "password": password,
             "email": email
-        }).then(response => {
-            console.log("post",response);
-            console.log(response.status);
-            return (response.status === 201);
         })
+        console.log("post",response);
+        console.log(response.status);
+        return (response.status === 201);
     }
 };
