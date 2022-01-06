c9f6e64f66a044f59ac0135aa587d596ec0f61eb
Lenovo <haymatit@post.bgu.ac.il>
2022-01-06 15:01:44 +0200

    report hours

--- reactfirst/src/components/register/requests.js
+++ reactfirst/src/components/register/requests.js
@@ -9,7 +9,7 @@ mock.onPost(SERVER_ADDRESS+'/users/register/student').reply(201, 'A new user has
 // mock.onPost(serverAddress+'/users/register/student').reply(400, 'A user with the same username already exists');
 // mock.onPost(serverAddress+'/users/register/student').reply(500, 'Server internal error');
 
-export function sendDetailsToServer({userType, username, firstname, lastname, email, password}){
+export function sendDetailsToServer(userType, username, firstname, lastname, email, password){
     // need to hash the password in the server, suppose to return error if username exists (or other)
     if(userType === "סטודנט"){
         axios.post(SERVER_ADDRESS+'/users/register/student',
