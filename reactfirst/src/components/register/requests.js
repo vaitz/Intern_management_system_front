f29872f49e667e36c96c01c7bb0d5edd5f5a4446
vaitz <vaitz.post.bgu.ac.il>
2022-01-11 21:20:22 +0200

    change axios to fetch in register

--- reactfirst/src/components/register/requests.js
+++ reactfirst/src/components/register/requests.js
@@ -1,33 +1,9 @@
-import axios from "axios";
-// import MockAdapter from 'axios-mock-adapter';
+import fetchMock from 'fetch-mock';
 import {SERVER_ADDRESS} from '../../config'
 
+fetchMock.mock(SERVER_ADDRESS+'/users/register/student', 201);
 
-// mocks
-// const mock = new MockAdapter(axios);
-// mock.onPost(SERVER_ADDRESS+'/users/register/student').reply(201, 'A new user has been added');
-// mock.onPost(serverAddress+'/users/register/student').reply(400, 'A user with the same username already exists');
-// mock.onPost(serverAddress+'/users/register/student').reply(500, 'Server internal error');
-
-// export function sendDetailsToServer(userType, username, firstname, lastname, email, password){
-//     // need to hash the password in the server, suppose to return error if username exists (or other)
-//     if(userType === "סטודנט"){
-//         axios.post(SERVER_ADDRESS+'/users/register/student',
-//         {
-//             "username": username,
-//             "firstName": firstname,
-//             "lastName": lastname,
-//             "password": password,
-//             "email": email
-//         }).then(response => {
-//             console.log("post",response);
-//             console.log(response.status);
-//             return (response.status === 201);
-//         })
-//     }
-// };
-
-export function sendDetailsToServer(userType, username, firstname, lastname, email, password){
+export function sendDetailsToServer({userType, username, firstname, lastname, email, password}){
     // need to hash the password in the server, suppose to return error if username exists (or other)
     if(userType === "סטודנט"){
         const data = {
@@ -37,7 +13,8 @@ export function sendDetailsToServer(userType, username, firstname, lastname, ema
             "password": password,
             "email": email
         }
-        fetch(SERVER_ADDRESS+'/users/register/student',
+        console.log(data);
+        const response =  fetch(SERVER_ADDRESS+'/users/register/student',
             {
                 method: 'POST',
                 mode: "cors",
@@ -49,25 +26,3 @@ export function sendDetailsToServer(userType, username, firstname, lastname, ema
         })
     }
 };
-
-// export async function sendDetailsToServer({userType, username, firstname, lastname, email, password}){
-//     // need to hash the password in the server, suppose to return error if username exists (or other)
-//     if(userType === "סטודנט"){
-//         console.log(SERVER_ADDRESS+'/users/register/student');
-//         await axios
-//         ({
-//             method: 'post',
-//             url: SERVER_ADDRESS + '/users/register/student',
-//             data: {
-//                     "username": username,
-//                     "firstName": firstname,
-//                     "lastName": lastname,
-//                     "password": password,
-//                     "email": email
-//                 }}).then(response => {
-//             console.log("post",response);
-//             console.log(response.status);
-//             return (response.status === 201);
-//         })
-//     }
-// };
