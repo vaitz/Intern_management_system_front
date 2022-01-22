8b9d3c779198b5d926fb73d010ef2f2d7c30ff54
vaitz <vaitz.post.bgu.ac.il>
2022-01-22 19:06:19 +0200

    Add request to get the priorities amount

--- reactfirst/src/components/internships_priorities/internships_priorities.js
+++ reactfirst/src/components/internships_priorities/internships_priorities.js
@@ -1,3 +1,33 @@
-// get internships to present
-// choose internships
-// saze to DB the prioritiesed internships
+import React, {useEffect, useState} from 'react';
+import {getInternships, getPrioritiesAmount, sendInternshipsToServer} from "./requests";
+
+const program = "program1"
+
+const InternshipsPriorities = () => {
+    const [options, setOptions] = useState([]);
+    const [prioritiesAmount, setPrioritiesAmount] = useState([]);
+
+    useEffect( () => {
+        getInternships(program, setOptions).then(r => console.log(r));
+        getPrioritiesAmount(program, setPrioritiesAmount).then(r => console.log(r));
+   }, [])
+
+
+// const onclick = (option) => {
+
+// }
+    return (
+        <div>
+            <ul>
+                {
+                    options.map((option, index)=>(
+                        <li key={index}>{option.companyName} - {option.internshipName}</li>
+                    ))
+                }
+            </ul>
+        </div>
+    );
+};
+
+export default InternshipsPriorities;
+
--- reactfirst/src/components/internships_priorities/requests.js
+++ reactfirst/src/components/internships_priorities/requests.js
@@ -1,14 +1,28 @@
 import fetchMock from 'fetch-mock';
 import {SERVER_ADDRESS} from '../../config'
 
-fetchMock.mock(SERVER_ADDRESS+'/internships/program1', [{'companyName': 'companyA','internshipName':'internshipA', 'about':'bla', 'requirments':'askdnaskn'}]);
+fetchMock.mock(SERVER_ADDRESS+'/internships/program1', [
+    {'companyName': 'companyA','internshipName':'internshipA', 'about':'bla', 'requirments':'askdnaskn'},
+    {'companyName': 'companyB','internshipName':'internshipB', 'about':'bla', 'requirments':'askdnaskn'},
+    {'companyName': 'companyC','internshipName':'internshipC', 'about':'bla', 'requirments':'askdnaskn'},
+]);
+fetchMock.mock(SERVER_ADDRESS+'/prioritiesAmount/program1', 3);
 fetchMock.mock(SERVER_ADDRESS+'/candidate/internshipsPriorities', 200);
 
 
-export const getInternships = (program, setOptions) => fetch(SERVER_ADDRESS+'/internships/'+program)
-    .then((response) => {
-        response.json().then(data => setOptions(data));
-    })
+export const getInternships = (program, setOptions) => 
+    fetch(SERVER_ADDRESS+'/internships/'+program)
+        .then((response) => {
+            response.json().then(data => setOptions(data));
+        })
+
+export const getPrioritiesAmount = (program, setPrioritiesAmount) => 
+    fetch(SERVER_ADDRESS+'/prioritiesAmount/'+program)
+        .then((response) => {
+            response.json().then(data => setPrioritiesAmount(data));
+        })
+
+        
 
 export function sendInternshipsToServer(username, prioritiesArr){
     // prioritiesArr- suppose to be array of string order by the priorities of the user
