77bbf6ea846e1226b49ca0b50ee4678ff30cee32
vaitz <vaitz.post.bgu.ac.il>
2022-02-05 19:50:32 +0200

    add constats for hebrew user types

---
+++ reactfirst/src/users/guest/register/constants.js
@@ -0,0 +1,5 @@
+export const STUDENT_HEBREW = "סטודנט";
+export const COMPANY_REPRESENTATIVE_HEBREW = "נציג חברה";
+export const MENTOR_HEBREW = "מנטור";
+export const PROGRAM_MANAGER_HEBREW = "מנהל תוכנית התמחות";
+export const PROGRAM_COORDINATOR_HEBREW = "רכז תוכנית התמחות";
