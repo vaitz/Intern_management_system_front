1bfa02811e2d03f49eac6518b6ac4d602c6f4635
vaitz <vaitz.post.bgu.ac.il>
2022-02-05 19:51:33 +0200

    add internships prorities

--- reactfirst/src/users/candidate/internships_priorities/internships_priorities.js
+++ reactfirst/src/users/candidate/internships_priorities/internships_priorities.js
@@ -1,30 +1,62 @@
 import React, {useEffect, useState} from 'react';
 import {getInternships, getPrioritiesAmount, sendInternshipsToServer} from "./requests";
+import Dropdown from "../../../components/dropdown";
+import Button from "../../../components/button";
+import styled from "styled-components";
 
+const ButtonWrapper = styled.div`
+  margin: 150px 400px 200px;
+`
 const program = "program1"
+const username = "may"
+// todo: get the program and the username from the cookie
 
 const InternshipsPriorities = () => {
     const [options, setOptions] = useState([]);
-    const [prioritiesAmount, setPrioritiesAmount] = useState([]);
+    const [prioritiesAmount, setPrioritiesAmount] = useState(1);
+    const [selected, setSelected] = useState([]);
+    const drops = []
 
-    useEffect( () => {
-        getInternships(program, setOptions).then(r => console.log(r));
-        getPrioritiesAmount(program, setPrioritiesAmount).then(r => console.log(r));
-   }, [])
+    useEffect(() => {
+        getPrioritiesAmount(program, setPrioritiesAmount);
+        getInternships(program, setOptions);
+    }, [])
 
+    const createDropDown = () => {
+        for (var i=1; i <= prioritiesAmount; i++) {
+            drops.push(<p key={i}> עדיפות {i}-</p>);
+            drops.push(<Dropdown options={options} onChange={handleChange} placeholder={"בחר התמחות"} width='200px' height="100px"/>);
+        }
+        return drops;
+    }
 
-// const onclick = (option) => {
+    const handleChange = (chosenOption) => {
+        // pop from options
+        setOptions(options.filter((option)=> {return option !== chosenOption}));
+        // add to selected
+        setSelected([...selected, chosenOption]);
 
-// }
+        // todo: remove the last value from selected
+
+        // todo: add to options
+
+        // todo: make sure the selected in order of priorities
+
+    }
+
+    const handleClick = () => {
+        sendInternshipsToServer(username, selected);
+        console.log(username);
+        console.log(selected);
+    }
+
     return (
         <div>
-            <ul>
-                {
-                    options.map((option, index)=>(
-                        <li key={index}>{option.companyName} - {option.internshipName}</li>
-                    ))
-                }
-            </ul>
+            <p>נא לבחור את ההתמחויות לפי סדר עדיפות</p>
+            {createDropDown()}
+            <ButtonWrapper>
+                <Button value={"שלח עדיפויות"} onClick={handleClick}/>
+            </ButtonWrapper>
         </div>
     );
 };
--- reactfirst/src/users/candidate/internships_priorities/requests.js
+++ reactfirst/src/users/candidate/internships_priorities/requests.js
@@ -6,22 +6,39 @@ fetchMock.mock(SERVER_ADDRESS+'/internships/program1', [
     {'companyName': 'companyB','internshipName':'internshipB', 'about':'bla', 'requirments':'askdnaskn'},
     {'companyName': 'companyC','internshipName':'internshipC', 'about':'bla', 'requirments':'askdnaskn'},
 ]);
-fetchMock.mock(SERVER_ADDRESS+'/prioritiesAmount/program1', 3);
+fetchMock.mock(SERVER_ADDRESS+'/prioritiesAmount/program1', JSON.stringify(3));
 fetchMock.mock(SERVER_ADDRESS+'/candidate/internshipsPriorities', 200);
 
+export const getPrioritiesAmount = (program, setPrioritiesAmount) => {
+    fetch(SERVER_ADDRESS+'/prioritiesAmount/'+program,
+        {
+            method: 'Get',
+            mode: "cors",
+        }).then(response => response.json().then(data => {
+            console.log(data);
+            setPrioritiesAmount(data);
+        }
+    ).catch(error => {
+        console.log("error");
+    }));
+}
 
-export const getInternships = (program, setOptions) => 
-    fetch(SERVER_ADDRESS+'/internships/'+program)
-        .then((response) => {
-            response.json().then(data => setOptions(data));
-        })
-
-export const getPrioritiesAmount = (program, setPrioritiesAmount) => 
-    fetch(SERVER_ADDRESS+'/prioritiesAmount/'+program)
-        .then((response) => {
-            response.json().then(data => setPrioritiesAmount(data));
-        })
-
+export const getInternships = (program, setOptions) => {
+    fetch(SERVER_ADDRESS+'/internships/'+program,
+        {
+            method: 'Get',
+            mode: "cors",
+        }).then((response) => {
+            response.json().then(data => {
+                console.log(data);
+                setOptions(data.map((obj, index)=>{
+                    return {value: index, label: obj.companyName + " " + obj.internshipName}
+                }))
+            });
+        }).catch(error => {
+            console.log("error");
+        });
+}
         
 
 export function sendInternshipsToServer(username, prioritiesArr){
@@ -29,12 +46,12 @@ export function sendInternshipsToServer(username, prioritiesArr){
     const data = {
         "username": username,
         "priorities": prioritiesArr,
-    }
-    const response = fetch(SERVER_ADDRESS+'/candidate/internshipsPriorities',
+    };
+    fetch(SERVER_ADDRESS+'/candidate/internshipsPriorities',
         {
             method: 'POST',
             mode: "cors",
             body: JSON.stringify(data)
-        }).then(response => response.json().then(data => console.log(data)));
+        }).then(response => response.json().then(data => {console.log(data)}));
 
-};
+}
