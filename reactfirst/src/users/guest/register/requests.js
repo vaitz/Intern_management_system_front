8173b0e832601be834eef8a42d8fb42843055453
vaitz <vaitz.post.bgu.ac.il>
2022-03-11 16:59:52 +0200

    Add program to student register

--- reactfirst/src/users/guest/register/requests.js
+++ reactfirst/src/users/guest/register/requests.js
@@ -8,18 +8,37 @@ fetchMock.mock(SERVER_ADDRESS+'/users/register/companyRep', 201);
 fetchMock.mock(SERVER_ADDRESS+'/users/register/mentor', 201);
 fetchMock.mock(SERVER_ADDRESS+'/users/register/programCoordinator', 201);
 fetchMock.mock(SERVER_ADDRESS+'/users/register/programManager', 201);
+fetchMock.mock(SERVER_ADDRESS+'/activePrograms', ["starships","פסיכולוגיה"]);
 
-export async function sendDetailsToServer({userType, username, firstname, lastname, email, password, companyName}){
+// get programs
+export const getPrograms = (setPrograms) => {
+    fetch(SERVER_ADDRESS+'/activePrograms',
+        {
+            method: 'Get',
+            mode: "cors",
+        }).then((response) => {
+        response.json().then(data => {
+            let tempData = [""];
+            tempData.push(...data);
+            setPrograms(tempData);
+        });
+    }).catch(error => {
+        console.log("error");
+    });
+}
+
+export async function sendDetailsToServer({userType, username, firstname, lastname, email, password, companyName, program}){
     // need to hash the password in the server, suppose to return error if username exists (or other)
-    var data = null;
-    var endpoint_address = null;
+    let data = null;
+    let endpoint_address = null;
     if(userType === STUDENT_HEBREW){
         data = {
             "username": username,
             "first_name": firstname,
             "last_name": lastname,
             "password": password,
-            "email": email
+            "email": email,
+            "program": program
         }
         
         endpoint_address = SERVER_ADDRESS+'/users/register/student';
