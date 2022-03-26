b10355b5168834a4276df3b9de7282a6f202f29f
Lenovo <haymatit@post.bgu.ac.il>
2022-03-26 17:35:08 +0300

    fix report hours

--- reactfirst/src/users/intern/report_hours/requests.js
+++ reactfirst/src/users/intern/report_hours/requests.js
@@ -1,7 +1,7 @@
 import {SERVER_ADDRESS} from '../../../config'
 import fetchMock from "fetch-mock";
 
-export const getWorkingHours = (userName, setHours) => fetch(SERVER_ADDRESS+`/internHours`,
+export const getWorkingHours = (username, setHours) => fetch(SERVER_ADDRESS+`/intern/getHours/${username}`,
     {
         method: 'Get',
         mode: "cors",
@@ -10,8 +10,8 @@ export const getWorkingHours = (userName, setHours) => fetch(SERVER_ADDRESS+`/in
             setHours(data);
     })))
 
-const data = JSON.stringify([{date: "1/2/2022", start: "09:00", end: "11:00"}, {date: "2/2/2022", start: "08:00", end: "13:00"}]);
-fetchMock.mock(SERVER_ADDRESS+'/internHours', data);
+const data = JSON.stringify([{date: "1/2/2022", startTime: "09:00", endTime: "11:00"}, {date: "2/2/2022", startTime: "08:00", endTime: "13:00"}]);
+fetchMock.mock(SERVER_ADDRESS+`/intern/getHours/user`, data);
 
 const bla = [
     { name: "חי מתתיהו"},
@@ -23,3 +23,23 @@ const bla = [
 ];
 // fetchMock.mock(SERVER_ADDRESS+'/getCompanies', companies);
 fetchMock.mock(SERVER_ADDRESS+'/company', bla);
+
+// todo: interns/hoursReport, fix first request
+
+export const reportHours = (username, hours) => {
+    const data = {
+        "username": username,
+        "hours": hours
+    }
+    fetch(SERVER_ADDRESS+`/intern/hoursReport`,
+    {
+        method: 'POST',
+        mode: "cors",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(response => console.log(response));}
+
+fetchMock.mock(SERVER_ADDRESS+'/intern/hoursReport', {status: 200});
