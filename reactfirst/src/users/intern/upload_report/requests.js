fc2c4e58f7edd387c2272840004577d5f385f60e
May <mayvaitz@gmail.com>
2022-04-23 17:01:39 +0300

    merge master

--- reactfirst/src/users/intern/upload_report/requests.js
+++ reactfirst/src/users/intern/upload_report/requests.js
@@ -2,7 +2,7 @@ import {SERVER_ADDRESS} from "../../../config";
 
 export const sendFile = (username, report) => {
 
-    return fetch(SERVER_ADDRESS+` /intern/uploadReport/${username}`,
+    return fetch(SERVER_ADDRESS+`/intern/uploadReport/${username}`,
         {
             method: 'POST',
             mode: "cors",
