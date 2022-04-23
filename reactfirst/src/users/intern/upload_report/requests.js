e6b712838cdd6e9afa9cd4fbf95f71f0f5aab416
May <mayvaitz@gmail.com>
2022-04-23 15:57:40 +0300

    merge master

--- reactfirst/src/users/intern/upload_report/requests.js
+++ reactfirst/src/users/intern/upload_report/requests.js
@@ -1,5 +1,4 @@
 import {SERVER_ADDRESS} from "../../../config";
-import fetchMock from "fetch-mock";
 
 export const sendFile = (username, report) => {
     const data = {
@@ -20,5 +19,3 @@ export const sendFile = (username, report) => {
             body: JSON.stringify(data)
         }).then(response => console.log(response));
 }
-
-fetchMock.mock(SERVER_ADDRESS + '/intern/uploadReport', {status: 200});
