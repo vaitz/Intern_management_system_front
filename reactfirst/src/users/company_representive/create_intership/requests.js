148b3422d2e2882992bc9fdee775bbbdb0d16fb5
vaitz <vaitz.post.bgu.ac.il>
2022-05-28 19:52:00 +0300

    Add mentor to create internship for company rep

--- reactfirst/src/users/company_representive/create_intership/requests.js
+++ reactfirst/src/users/company_representive/create_intership/requests.js
@@ -1,13 +1,14 @@
 import fetchMock from "fetch-mock";
 import {SERVER_ADDRESS} from '../../../config'
 
-export const createInternship = (setPopup,setError,program,internshipName,internshipDescription,demands, username) => {
+export const createInternship = (setPopup,setError,program,internshipName,internshipDescription,demands,username,mentor) => {
     const data = {
         "program": program,
         "internshipName": internshipName,
         "about": internshipDescription,
         "requirements": demands,
         "username": username,
+        "mentor": mentor,
     }
     const response = fetch(SERVER_ADDRESS+'/companyRep/createInternship',
         {
@@ -20,7 +21,6 @@ export const createInternship = (setPopup,setError,program,internshipName,intern
             body: JSON.stringify(data)
         })
         .then(response => {
-            console.log(response);
             if (response.status === 200) setPopup(true);
             else if (response.status === 400) setError("שם ההתמחות קיים כבר במערכת, יש לבחור שם אחר");
             else if (response.status === 404) setError("החברה או התוכנית לא קיימים במערכת, נסו שוב");
@@ -47,6 +47,33 @@ export const getPrograms = (setPrograms) => {
     });
 }
 
+
+export const getMentors = (setMentors,username) => {
+    fetch(SERVER_ADDRESS+`/mentorsByCompanyRep/${username}`,
+        {
+            method: 'Get',
+            mode: "cors",
+        }).then((response) => {
+        response.json().then(data => {
+            let tempData = [{key: "", value:""}];
+            let names = data.map((mentor) => ({
+                key: mentor.username,
+                value: mentor.firstName + " " + mentor.lastName
+            }));
+            tempData.push(...names);
+            setMentors(tempData);
+        });
+    }).catch(error => {
+        console.log(error);
+    });
+}
+
 const data = ["123", "2", "3"]
+const mentors = [
+    { firstName: "חי", lastName: "מתתיהו", username: "ff" },
+    { firstName: "יובל", lastName: "מור", username: "aa"},
+];
+
+fetchMock.mock(SERVER_ADDRESS+`/mentorsByCompanyRep/user` , mentors);
 fetchMock.mock(SERVER_ADDRESS+'/companyRep/createInternship', "success");
 fetchMock.mock(SERVER_ADDRESS+'/activePrograms', data);
